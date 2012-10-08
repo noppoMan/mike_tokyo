01924b2198a6e5d89f15400fc1556056aece62c4
Yuki Takei <yukiofsunrising.ver.2@gmail.com>
2012-10-09 01:19:50 +0900

    development環境構築

--- app/config/bootstrap.js
+++ app/config/bootstrap.js
@@ -3,7 +3,12 @@ var bootstrap = {
 	,init : function(req, res){
 		//console.log(req.headers.host);
 		switch(req.headers.host){
+			case "localhost:8888":
+			case "localhost":
+				this.env = "localhost"
+				break;
 			default:
+				this.env = "development";
 				break;
 		}
 
